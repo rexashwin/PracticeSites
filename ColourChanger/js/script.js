e5e7431f2f75673473fb071b71b49f65baee53fd
Ashwin Raj <rajashwin1163@gmail.com>
2024-07-04 00:36:20 +0530

    bg-colourChanger-OnClick

---
+++ ColourChanger/js/script.js
@@ -0,0 +1,14 @@
+function redFunc() {
+    document.body.style.backgroundColor = "red";
+    document.body.style.color = "white";
+}
+
+function whiteFunc() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
+
+function blueFunc() {
+    document.body.style.backgroundColor = "blue";
+    document.body.style.color = "white";
+}
