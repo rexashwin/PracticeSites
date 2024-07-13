fa3979826d3e028016a3b2ee7f6a0bbc87c4ca33
Ashwin Raj <rajashwin1163@gmail.com>
2024-07-13 20:04:58 +0530

    guessNum gameplan

--- GuessNumber/js/script.js
+++ GuessNumber/js/script.js
@@ -18,64 +18,6 @@ let guessLimit = 10;
 
 let playGame = true;
 
-if (playGame) {
-    submit.addEventListener('click', function (e) {
-        e.preventDefault();
-        // get the user value in int
-        let guess = parseInt(userInput.value);
-        // console.log(guess);
-        validateGuess(guess);
-    });
-}
-
-// validate the guess value 
-function validateGuess(guess) {
-    if (isNaN(guess)) {
-        alert("Please enter a valid number.");
-    }
-    else if (guess < 1 || guess > 100) {
-        alert("Please enter a number between 1 to 100");
-    }
-    // if the guess value is okay
-    else {
-        // last guessed is pushes to array so can be displayed to user
-        prevGuess.push(guess);
-        // check the limit for game play
-        if (numGuess > guessLimit) {
-            displayGuess(guess);
-            displayMessage(`Game over! Random number was ${randomNumber}`);
-            endGame();
-        }
-        else {
-            displayGuess(guess);
-            checkGuess(guess);
-        }
-    }
-}
-
-// check the user value with random num and end the game
-function checkGuess(guess) {
-    if (guess === randomNumber) {
-        displayMessage(`You guessed it Right! \n Random number was ${randomNumber}`);
-        endGame();
-    }
-}
-
-function displayGuess(guess) {
-    //
-}
-
-function displayMessage(message) {
-    // clear the user value
-    userInput.value = ``;
-    // display the last guesses to user
-    guessSlot.innerHTML += `${guess}`;
-    // num of times guessed
-    numGuess++;
-    // remaining times to guess
-    remaining.innerHTML = `${guessLimit - numGuess}`;
-}
-
 function endGame() {
     // 
 }
