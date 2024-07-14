9db10a507e70e495d7434126ccebc125f078171a
Ashwin Raj <rajashwin1163@gmail.com>
2024-07-14 23:20:58 +0530

    guessNum feat: start and validate guess value

--- GuessNumber/js/script.js
+++ GuessNumber/js/script.js
@@ -4,7 +4,7 @@ let randomNumber = parseInt(Math.random() * 100 + 1);
 
 
 let submit = document.getElementById("submit");
-let guessField = document.querySelector("#guessField");
+let userInput = document.querySelector("#guessField");
 let guessSlot = document.querySelector(".guessSlot");
 let remaining = document.querySelector(".remaining");
 let lowOrHi = document.querySelector(".lowOrHi");
@@ -12,3 +12,37 @@ let startOver = document.querySelector(".resultParas");
 
 let p = document.createElement("p");
 
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault();
+        let guess = parseInt(userInput.value);
+        // console.log(guess);
+        validateGuess(guess);
+    });
+}
+
+function validateGuess(guess) {
+    if(isNaN(guess)){
+        alert("Please enter a valid number.");
+    }
+    else if(guess < 1 || guess > 100){
+        alert("Please enter a number between 1 to 100");
+    }
+}
+
+function checkGuess(guess) {
+    //
+}
+
+function displayGuess(guess) {
+    //
+}
+
+function displayMessage(message) {
+    //
+}
