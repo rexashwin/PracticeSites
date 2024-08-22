a4b0006f85d6688fb9b45028dfc999e32bfa7ba2
Ashwin Raj <rajashwin1163@gmail.com>
2024-08-22 17:29:10 +0530

    09 bug - randomColor value return

--- 09_infiniteColorChanger/script.js
+++ 09_infiniteColorChanger/script.js
@@ -1,15 +1,15 @@
-function randomColor() {
+const randomColor = function () {
     let hex = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16)];
     }
-    console.log(color);
+    return color;
 }
 
 function startChangingColor() {
     setInterval(changeBgColor(), 1000);
-
+    
     function changeBgColor() {
         document.body.style.backgroundColor = randomColor();
     }
