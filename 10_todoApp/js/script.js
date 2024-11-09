9ed5cb86eed489976e6eb17cdc0216b01809696d
Ashwin Raj <rajashwin1163@gmail.com>
2024-11-09 19:04:21 +0530

    10 js: update item list

--- 10_todoApp/js/script.js
+++ 10_todoApp/js/script.js
@@ -1,5 +1,9 @@
+let todoList = [`buy milk`]
+
+
 function addTodo() {
     let todoInputElement = document.querySelector(`#todo-input`);
     let todoInputValue = todoInputElement.value;
     console.log(todoInputValue);
+    todoList.push(todoInputValue);
 }
